cca7851bc2efdd8af82c749481371cb1ed0ced18
Subho Shankar Basu <subho.basu@ugent.be>
2022-12-26 20:11:37 +0100

    Cleanup

--- main.cpp
+++ main.cpp
@@ -5,14 +5,17 @@
  *      Author: Administrator
  */
 
-
 #include "temperature_controller_module.h"
+#include "config.h"
+#include <iostream>
 
 int main_test(int argc, char **argv);
 int main_deploy(int argc, char **argv);
 
 int main(int argc, char **argv)
 {
+	std::cout << "Version " << Automatic_temperature_controller_VERSION_MAJOR << "." << Automatic_temperature_controller_VERSION_MINOR << std::endl;
+
 	if(UNIT_TESTING_ENABLED)
 		main_test(argc, argv);
 	else
@@ -20,3 +23,4 @@ int main(int argc, char **argv)
 
 	return 0;
 }
+
